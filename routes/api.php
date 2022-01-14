b97fc5c7ab996d63c31b46007512af15d4e79070
endritsusurii <62879078+endritsusurii@users.noreply.github.com>
2022-01-14 20:50:41 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Klientet;
 use App\Models\Porosite;
 use App\Http\Controllers\ShikoporositeController;
+use App\Http\Controllers\KlientetController;
+use App\Http\Controllers\PorositeController;
+use App\Http\Controllers\Detajet_e_porosiseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +30,9 @@ Route::get('/porosite', [ShikoporositeController::class, 'index']);
 Route::post('/pagesat', [ShikoporositeController::class, 'store']);
 Route::put('/perditso_pagesen/{id}', [ShikoporositeController::class, 'update']);
 Route::delete('/fshi_pagesat/{id}', [ShikoporositeController::class, 'destroy']);
+
+Route::get('/klientet/{id}', [KlientetController::class, 'show']);
+
+Route::get('/klientet/{id}/porosite', [PorositeController::class, 'show']);
+
+Route::get('/klientet/{id}/detajet_e_porosise', [Detajet_e_porosiseController::class, 'show']);
