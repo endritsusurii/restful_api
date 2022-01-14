9070d9322405373da33c2fe275307a20718a2748
endritsusurii <62879078+endritsusurii@users.noreply.github.com>
2022-01-14 21:20:07 +0100

    update

--- database/seeders/ProduktetTableSeeder.php
+++ database/seeders/ProduktetTableSeeder.php
@@ -19,7 +19,7 @@ class ProduktetTableSeeder extends Seeder
             DB::table('produktets')->insert([
                 'id_detajet_e_produkteve' => '1',
                 'emri_produktit' => 'Maus Razer DeathAdder Essential (2021), i zi',
-                'pershkrimi_i_produktit' => 'Mausi i lojërave Razer DeathAdder Essential është krijuar për dorën e djathtë. Ka një sensor me një ndjeshmëri deri në 6400 DPI. Sensori trajton nxitimin deri në 30 G, shpejtësinë 220 inç në sekondë dhe koha e përgjigjes është 1000Hz / 1ms. Ka gjithsej 5 butona të programueshëm. Mausi është i lidhur me kabllo, i krijuar për USB dhe lidhësi i tij është i veshur me ar.',
+                'pershkrimi_i_produktit' => 'Mausi i lojërave Razer DeathAdder Essential.',
                 'sasia_ne_stok' => '10',
                 'cmimi' => '50',
                 'created_at' => date("Y-m-d H:i:s"),
